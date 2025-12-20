8fddd152423bf2e49b7ae596da462fe944e9b9a0
Filipe Toscano <filipe.toscano@gmail.com>
2025-12-20 23:50:33 +0300

    Add cli project

---
+++ tools/Lefty.Powerpoint.Cli/CleanCommand.cs
@@ -0,0 +1,26 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+
+namespace Lefty.Powerpoint.Cli;
+
+/// <summary />
+[Command( "clean" )]
+public class CleanCommand
+{
+    private readonly PowerpointCleanerService _svc;
+
+
+    /// <summary />
+    public CleanCommand( PowerpointCleanerService svc )
+    {
+        _svc = svc;
+    }
+
+
+    /// <summary />
+    public async Task<int> OnExecuteAsync()
+    {
+        await _svc.CleanAsync( "Presentation1.pptx", "Presentation2.pptx" );
+
+        return 0;
+    }
+}
---
+++ tools/Lefty.Powerpoint.Cli/Program.cs
@@ -0,0 +1,78 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Lefty.Powerpoint.Cli;
+
+/// <summary />
+[Command( "pptxdo" )]
+[Subcommand( typeof( CleanCommand ))]
+public class Program
+{
+    /// <summary />
+    public static int Main( string[] args )
+    {
+        /*
+         * 
+         */
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .CreateLogger();
+
+        var logger = Log.ForContext<Program>();
+
+
+        /*
+         * 
+         */
+        var svc = new ServiceCollection();
+
+        svc.AddLogging( loggingBuilder =>
+            loggingBuilder.AddSerilog( dispose: true ) );
+
+        svc.AddTransient<PowerpointCleanerService>();
+
+        var sp = svc.BuildServiceProvider();
+
+
+        /*
+         * 
+         */
+        var app = new CommandLineApplication<Program>();
+
+        try
+        {
+            app.Conventions
+                .UseDefaultConventions()
+                .UseConstructorInjection( sp );
+        }
+        catch ( Exception ex )
+        {
+            logger.Fatal( ex, "Unhandled exception" );
+
+            return 2;
+        }
+
+
+        /*
+         * 
+         */
+        try
+        {
+            return app.Execute( args );
+        }
+        catch ( UnrecognizedCommandParsingException ex )
+        {
+            logger.Error( ex, "Unrecognized command" );
+
+            return 2;
+        }
+        catch ( Exception ex )
+        {
+            logger.Fatal( ex, "Unhandled exception" );
+
+            return 2;
+        }
+    }
+}
