c6b650c16a43d2e3d9a9d96107e7b423f6b98c6b
Filipe Toscano <filipe.toscano@gmail.com>
2025-12-20 23:49:09 +0300

    Remove hidden slides, remove notes

---
+++ src/Lefty.Powerpoint/PowerpointCleanResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Lefty.Powerpoint;
+
+/// <summary />
+public class PowerpointCleanResult
+{
+    /// <summary>
+    /// Number of slide masters.
+    /// </summary>
+    public int SlideMasters { get; set; }
+
+    /// <summary>
+    /// Number of removes slides.
+    /// </summary>
+    public int RemovedSlides { get; set; }
+}
---
+++ src/Lefty.Powerpoint/PowerpointCleanerOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Lefty.Powerpoint;
+
+/// <summary />
+public class PowerpointCleanerOptions
+{
+    /// <summary />
+    public bool RemoveHidden { get; set; } = true;
+
+    /// <summary />
+    public bool RemoveNotes { get; set; } = true;
+
+    /// <summary />
+    public bool RemoveComments { get; set; } = true;
+}
